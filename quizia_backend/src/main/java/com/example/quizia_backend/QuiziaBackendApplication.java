0fa362cda80a0776dd55a1731af68b00dbd44ad7
Nixon Mburu <nixonwainaina@gmail.com>
2025-11-22 09:24:24 +0300

    Initial commit

---
+++ quizia_backend/src/main/java/com/example/quizia_backend/QuiziaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.quizia_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuiziaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuiziaBackendApplication.class, args);
+	}
+
+}
---
+++ quizia_backend/src/test/java/com/example/quizia_backend/QuiziaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quizia_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuiziaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
